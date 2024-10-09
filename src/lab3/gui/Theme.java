69feb3230a44539d87b828498930ff67fd36b750
Kevin27182 <kevinmnard@gmail.com>
2024-10-09 18:24:26 -0500

    Added color palette

--- src/lab3/gui/Theme.java
+++ src/lab3/gui/Theme.java
@@ -5,9 +5,33 @@
 
 package lab3.gui;
 
+import java.awt.*;
+
 public class Theme {
 
     public static String WINDOW_TITLE = "Wine Data Analysis";
     public static int WINDOW_WIDTH = 800;
     public static int WINDOW_HEIGHT = 600;
+    public static Color DARK_BACKGROUND_1 = new Color(0x2f3542);
+    public static Color DARK_BACKGROUND_2 = new Color(0x57606f);
+    public static Color DARK_BACKGROUND_3 = new Color(0x747d8c);
+    public static Color DARK_BACKGROUND_4 = new Color(0x747d8c);
+    public static Color LIGHT_BACKGROUND_1 = new Color(0xced6e0);
+    public static Color LIGHT_BACKGROUND_2 = new Color(0xdfe4ea);
+    public static Color LIGHT_BACKGROUND_3 = new Color(0xf1f2f6);
+    public static Color LIGHT_BACKGROUND_4 = new Color(0xffffff);
+    public static Color PINK = new Color(0xFF7F92);
+    public static Color RED = new Color(0xff4757);
+    public static Color TOMATO = new Color(0Xff6348);
+    public static Color CORAL = new Color(0Xff7f50);
+    public static Color ORANGE = new Color(0xffa502);
+    public static Color YELLOW = new Color(0xeccc68);
+    public static Color LIME = new Color(0x7FED7B);
+    public static Color GREEN = new Color(0x2ed573);
+    public static Color BLUE_1 = new Color(0x3742fa);
+    public static Color BLUE_2 = new Color(0x1e90ff);
+    public static Color BLUE_3 = new Color(0x70a1ff);
+    public static Color MAUVE = new Color(0x8684D5);
+    public static Color PURPLE = new Color(0x7B2FE8);
+    public static Color MAGENTA = new Color(0x9D5BB6);
 }
